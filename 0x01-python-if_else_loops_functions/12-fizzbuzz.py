c69c11e4b0bf98a7eb66f550a88e1491ea228551
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-09 04:21:15 +0300

    file test

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(0, 100):
+    for i in range(1, 100):
         if (i % 3 == 0) & (i % 5 == 0):
             flexVar = "FizzBuzz"
         elif (i % 3 == 0):
