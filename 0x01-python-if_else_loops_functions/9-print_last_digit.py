6267a462d5f95fbab39b13a4f75748996266d353
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-09 03:18:55 +0300

    9-print_last_digit.py file added

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = 10 - (number % 10)
+    else:
+        last = number % 10
+    print("{}".format(last), end="")
+
+    return(last)
