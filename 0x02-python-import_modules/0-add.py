11e2b197cec871f69d6eab82610e2c80278950a2
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-11 01:03:07 +0300

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,5 @@ if __name__ == "__main__":
     from add_0 import add
 
 first = 1
-second = 2
-addNum = add(first, second)
-print("{} + {} = {}".format(first, second, addNum))
+second = 2)
+print("{} + {} = {}".format(first, second, add(first, second)))
