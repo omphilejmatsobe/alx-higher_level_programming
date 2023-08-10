486012641d013b1c2ad5bf0361b980f27d3c6731
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-10 13:17:30 +0300

    file added

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+import add_0 as add
+
+first = 1
+second = 2
+addNum = add.add(first, second)
+print("{} + {} = {}".format(first, second, addNum))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+This is a readme for the 0x02-python-import_module
