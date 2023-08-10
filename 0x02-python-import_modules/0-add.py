030e930acff17386a29053a13b38e5cd84da67a6
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-11 01:12:21 +0300

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-
-first = 1
-second = 2
-print("{} + {} = {}".format(first, second, add(first, second)))
+    first = 1
+    second = 2
+    print("{} + {} = {}".format(first, second, add(first, second)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import calculator_1 as cal
+if __name__ == "__main__":
+    import calculator_1 as cal
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-num = cal.add(a, b)
-print("{} + {} = {}".format(a, b, num))
+    num = cal.add(a, b)
+    print("{} + {} = {}".format(a, b, num))
 
-num = cal.sub(a, b)
-print("{} - {} = {}".format(a, b, num))
+    num = cal.sub(a, b)
+    print("{} - {} = {}".format(a, b, num))
 
-num = cal.mul(a, b)
-print("{} * {} = {}".format(a, b, num))
+    num = cal.mul(a, b)
+    print("{} * {} = {}".format(a, b, num))
 
-num = cal.div(a, b)
-print("{} / {} = {}".format(a, b, num))
+    num = cal.div(a, b)
+    print("{} / {} = {}".format(a, b, num))
