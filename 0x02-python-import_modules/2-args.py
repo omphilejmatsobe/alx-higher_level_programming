9274113a5cb7957ee096e8b8008b4b5d4dabf8fa
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-10 14:56:40 +0300

    file updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,14 @@ if __name__ == "__main__":
 
     import sys
 
-if (len(sys.argv) - 1) >= 0:
-    if (len(sys.argv) - 1) == 1:
-        tag = "argument"
-    else:
-        tag = "arguments"
+    if (len(sys.argv) - 1) >= 0:
+        if (len(sys.argv) - 1) == 1:
+            tag = "argument"
+        else:
+            tag = "arguments"
 
-    if (len(sys.argv) - 1) == 0:
-        sign = "."
-    else:
-        sign = ":"
+        if (len(sys.argv) - 1) == 0:
+            sign = "."
+        else:
+            sign = ":"
     print("{} {}{}".format((len(sys.argv) - 1), tag, sign))
