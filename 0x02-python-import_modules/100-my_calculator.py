fc97655ef3d1e598e76c0b456c7e22e3c22339f1
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-12 02:07:16 +0300

    100-my_calculator file updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,20 +12,15 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    sn = 0
 
     if sys.argv[2] == "*":
         res = mul(a, b)
-        sn = 1
     elif sys.argv[2] == "+":
         res = add(a, b)
-        sn = 1
     elif sys.argv[2] == "-":
         res = sub(a, b)
-        sn = 1
-    elif argv[2] == "/":
+    elif sys.argv[2] == "/":
         res = div(a, b)
-        sn = 1
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
