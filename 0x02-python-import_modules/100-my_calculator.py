1357e7619a50bfba8a113ebc00f22d2a3e4eb9b2
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-12 01:14:37 +0300

    files added

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import calculator_1
+    import calculator_1 as cal
 
     numArg = len(sys.argv) - 1
 
-    if numArg < 3:
+    if numArg != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit (1)
+        sys.exit(1)
 
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == ("*" or "-" or "+" or "/"):
+        if argv[2] == "*":
+            res = cal.mul(a, b)
+        elif argv[2] == "+":
+            res = cal.add(a, b)
+        elif argv[2] == "-":
+            res = cal.sub(a, b)
+        elif argv[2] == "/":
+            res = cal.div(a, b)
+
+        print("{} {} {} = {}".format(a, argv[2], b, res))
+    else:
+        print("Unkwon operator. Available operator: +, -, * and /")
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,8 +6,8 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return(c)
+        return (c)
 
     else:
         subs = sub(a, b)
-        return(subs)
+        return (subs)
