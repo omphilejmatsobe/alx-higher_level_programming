d4c3745c664c09b84fb47360093c5d12378136dd
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-11 10:57:31 +0300

    file test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
-    first = 1
-    second = 2
-    print("{} + {} = {}".format(first, second, add(first, second)))
+    import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add.add(a, b)))
+
