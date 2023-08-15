d7917c652c4c36c1bd4ebaaa03e6ef5c868f7360
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-15 18:27:28 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,3 @@ def no_c(my_string):
         if my_string[i] != "C" and my_string[i] != "c":
             track += my_string[i]
     return (track)
-
-
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for x in range(len(matrix[i])):
+            if x < (len(matrix[i]) - 1):
+                print("{:d} ".format(matrix[i][x]), end="")
+            else:
+                print("{:d}".format(matrix[i][x]))
