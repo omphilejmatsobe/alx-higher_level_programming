076d9abed781682b44265e85669fa8ea3e47da07
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-29 02:33:42 +0300

    Exception error fixed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return (true)
-    except IndexError:
+    except (ValueError, TypeError):
         return (false)
