d95c01c7458f43b34650733687de5662a0f1930b
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-29 02:35:23 +0300

    Exception error fixedvim

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,6 @@ def safe_print_integer(value):
 
     try:
         print("{:d}".format(value))
-        return (true)
+        return (True)
     except (ValueError, TypeError):
-        return (false)
+        return (False)
