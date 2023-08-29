55e0b162d1a7ec1c94fe3e0a59802d1475389864
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-08-29 03:04:09 +0300

    safe print division implemented

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    
+
     try:
-       i = a / b
+        i = a / b
+
     except (ZeroDivisionError, TypeError):
-       i = None
+        i = None
+
     finally:
-       print("Inside result: {}".format(i))
+        print("Inside result: {}".format(i))
+
     return (i)
