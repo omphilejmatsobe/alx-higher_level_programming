96aef332d49f94e26ce44ea94d8d7d7201329b3a
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-09-05 01:45:19 +0300

    file added

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,9 +6,9 @@ Class rectangle
 
 class Rectangle:
     """Rectangle Class With attributes width and height"""
-    
+
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -40,6 +40,17 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
+
     def area(self):
         return (self.__width * self.__height)
 
