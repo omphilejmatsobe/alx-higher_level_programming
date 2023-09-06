b3b9da941f6cf16fbfa14cef1a60b400d2e8caec
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-09-06 06:10:22 +0300

    Locked Class file added

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
+"""Defines a class LockedClass"""
+
+
 class LockedClass:
+    """
+    Prevents creating new instance attributes,
+    except if user fist name is given
+
+    Attributes:
+        first_name (str): first name of something.
+    """
+
     __slots__ = ["first_name"]
+
+    def __init__(self):
+
+        """Creates new instances"""
+
+        self.first_name = "first_name"
