3318b702b4798696de305c894c507fb83d6f10a4
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-09-12 21:13:56 +0300

    file test

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,5 +3,16 @@
 import json
 
 
+"""
+=====================================
+Module with the method to_json_string
+=====================================
+"""
+
 def to_json_string(my_obj):
+        """returns the JSON representation of an object
+    Arguments:
+        param1: my_obj
+    Return: json representation"""
+
     return json.dumps(my_obj)
