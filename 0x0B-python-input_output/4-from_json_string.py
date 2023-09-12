f16c87f18b6776294fd1637ef271d8f1f687e924
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-09-12 21:19:11 +0300

    file test

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import json
+
+
 """
 =================================
 Module with the method read_lines
@@ -7,12 +10,11 @@ Module with the method read_lines
 """
 
 
-def append_write(filename="", text=""):
-    """returns the JSON representation of an object
+def from_json_string(my_str):
+    """returns an object (Python data structure) represented by a JSON string
     Arguments:
-        param1: filame
-        param2: text
-    Return: JSON representation"""
+        param1: my_str
+    Return: Object code of JSON"""
 
-    with open(filename, "a", encoding="UTF-8") as fn:
-        return fn.write(text)
+    def from_json_string(my_str):
+    return json.loads(my_str)
