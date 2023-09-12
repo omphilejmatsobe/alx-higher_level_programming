3c03e271faee7be3410bfdb470fb92ec4cb19ad9
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-09-12 21:06:08 +0300

    file test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,7 +7,7 @@ module of the method number_of_lines
 """
 
 
-def number_of_lines(filename="", text=""):
+def write_file(filename="", text=""):
     """Writes a string to a text file and returns the number of characters written
     Arguments:
         param1: filename
