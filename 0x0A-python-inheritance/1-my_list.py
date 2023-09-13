c021f0789ec5ef97baddc1bb64d1674c8d557c29
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-09-14 01:55:11 +0300

    file test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,12 +6,11 @@ Module with the method MyList
 =============================
 """
 
-class MyList(list):
-        """function inherits methods from list
-    Arguments:
-        param1: list
-    """
 
+class MyList(list):
+    """module inherits methods from list
+    Arguments:
+        param1: list"""
 
     def print_sorted(self):
         print(sorted(self))
