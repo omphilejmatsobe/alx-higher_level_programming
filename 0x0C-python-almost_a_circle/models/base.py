2688472d1b4a534ea5d9b112ca32cbe299f96082
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-09-19 01:12:59 +0300

    file test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ Module with class Base
 ======================
 """
 
+import json
 
 class Base:
     """Class Base for the Base"""
@@ -22,3 +23,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        that returns the JSON string representation of list_dictionaries
+        """
+
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
