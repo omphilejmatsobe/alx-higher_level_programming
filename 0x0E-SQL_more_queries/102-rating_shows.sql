744a74b30e53cad7274ddb1ffb01ae72174d6704
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-10-18 20:13:10 +0300

    file test

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- import the database hbtn_0d_tvshows_rate dump to the MySQL server
+SELECT name, SUM(rate) AS rating
+  FROM tv_genres AS genres
+       INNER JOIN tv_show_genres AS show_genres
+	   ON genres.id = show_genres.genre_id
+	   INNER JOIN tv_show_ratings AS ratings
+	   ON show_genres.show_id = ratings.show_id
+ GROUP BY name ORDER BY rating DESC;
