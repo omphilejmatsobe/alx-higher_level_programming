a7b3c3eb0a0bb90daa520851f75dbebdaa3b8b54
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-19 21:45:51 +0200

    File test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
     This is the module for the database collection of data
     """
 
-    d = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    d.cursor()
-    d.cursor().execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in d.cursor().fetchall()]
+    d = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], d=sys.argv[3])
+    c = d.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
