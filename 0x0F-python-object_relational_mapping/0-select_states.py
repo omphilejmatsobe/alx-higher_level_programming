6b64e856b0cc7fc81ef7b7186d2fe383baa785d3
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-19 21:38:24 +0200

    Documenation added

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+Module states
+"""
 
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
+    """
+    This is the module for the database collection of data
+    """
+
     d = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     d.cursor()
