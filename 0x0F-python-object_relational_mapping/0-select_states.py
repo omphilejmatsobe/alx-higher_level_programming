fdbc704954cd831cb0782cd6f312d8bb3d3c60b8
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-11-22 21:12:25 +0200

    File test

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-"""
-Module states
-"""
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+
 
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    """
-    This is the module for the database collection
-    """
-
-    d = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                        passwd=argv[2], db=argv[3])
-
-    cursor = db.cursor()
-    cur.execute("SELECT * FROM states")
-
-    i = cursor.fetchall()
-    for x in i:
-        print(x)
-
-    cur.close()
-    db.close()
+    """Class to select state"""
+    data = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], data=sys.argv[3])
+    c = data.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
