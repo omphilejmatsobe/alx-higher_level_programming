cb16d2c89f9ea80c19f87c3be9564e367cf4afa9
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-12-01 23:02:04 +0200

    File test

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    takes your Github credentials (username and password) and uses the
+    Github API to display your id
+    """
+    usr = argv[1]
+    passwrd = argv[2]
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(usr, passwrd))
+    try:
+        print(req.json().get('id'))
+    except:
+        pass
