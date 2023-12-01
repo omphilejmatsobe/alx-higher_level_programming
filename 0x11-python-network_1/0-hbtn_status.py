f6a327b3024ca05a945ca564ee6500e32ac905ef
omphilejmatsobe <omphilejmatsobe@gmail.com>
2023-12-01 22:55:52 +0200

    File test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,8 @@ import urllib.request
 
 if __name__ == "__main__":
     """fetches https://alx-intranet.hbtn.io/status"""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
         html_str = html.decode('utf-8')
 
     print("Body response:")
