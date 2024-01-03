213661cf049c44a3c6055e9791d00f1e1fcd10e7
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-01-03 23:21:57 +0200

    FIle check

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fileSystem = require('fs');
+
+fileSystem.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
