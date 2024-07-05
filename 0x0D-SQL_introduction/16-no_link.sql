c2b70f5c875292ead1fefe79ba926d613a883859
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-05 18:55:07 +0200

    This lists all records of a table. File update

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- This script lists all records of table --
-SELECT name, score FROM second_table WHERE name!='' AND name IS NOT NULL ORDER BY score DESC;
+SELECT name, score FROM second_table WHERE name!='' OR name IS NOT NULL ORDER BY score DESC;
