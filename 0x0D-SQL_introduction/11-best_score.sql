98a261677074786a945737436c2062db515cecba
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-05 18:31:57 +0200

    Best score script update

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- This is script lists all records with score >= 10 in table --
-SELECT * FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
