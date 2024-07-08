bd6483e8f35c7fa4b50af48f924b6e5c04ac1a93
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-08 15:25:01 +0200

    File check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
 
     import sys
-    
-    if len(sys.argv)  == 1:
+
+    if len(sys.argv) == 1:
         print("{} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
         print("{} argument:".format(len(sys.argv) - 1))
