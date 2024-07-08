71486997a9e4f938c6e6ae13f71a1bb70a51df24
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-08 14:28:13 +0200

    File check

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
     from add_0 import add
+
     a = 1
     b = 2
-    sumRes = add(a, b)
-    print(f"{a} + {b} = {sumRes}")
 
+    print("{} + {} = {}".format(a, b, add(a, b)))
