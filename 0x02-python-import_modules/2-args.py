8d096531a5aa1dcdd969550c132cae5fa105fa3a
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-08 15:20:02 +0200

    File check

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+    
+    if len(sys.argv)  == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+
+    for x in range(1, len(sys.argv)):
+        print("{}: {}".format(x, sys.argv[x]))
