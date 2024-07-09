613ec311acf0afa410fb7fd79beb8f9065e48401
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-09 23:29:45 +0200

    Getters and setters added

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """
+    Class defining a square
+    """
+    def __init__(self, size=0):
+        """
+        Method to initialize instance attributes
+        """
+
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            pass
+
+    def area(self):
+        """
+        Method that return area of Square object
+        """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
