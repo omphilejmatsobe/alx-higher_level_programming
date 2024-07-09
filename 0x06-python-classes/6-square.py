abc94cbc3f9b2538dc26c17380e7fbc703ae830e
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-09 23:57:21 +0200

    Square class update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,12 +9,13 @@ class Square:
     """
     Class defining a square
     """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=0):
         """
         Method to initialize instance attributes
         """
 
         self.__size = size
+        self.__position = position
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -31,10 +32,16 @@ class Square:
 
     @property
     def size(self):
+        """
+        Method used to retrieve size of the Square object
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Method used to set the size of the Square Object
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -42,7 +49,30 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """
+        Method used to retrieve the position attribute for Square
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Method used to set the position of square 
+        """
+
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
     def my_print(self):
+        """
+        Method used to print a Square
+        """
         if self.__size == 0:
             print("")
         else:
@@ -52,5 +82,3 @@ class Square:
                         print("#", end="")
                     else:
                         print("#")
-
-
