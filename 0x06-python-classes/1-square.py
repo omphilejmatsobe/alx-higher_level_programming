f0643a7a8367320cdf2a2707800f68d58a6b2f52
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-09 19:25:20 +0200

    File test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Class that defines a square
+"""
+
+
+class Square:
+    """
+    Class defining a square
+    """
+    def __init__(self, size):
+        """
+        Method to initialize instance attributes
+        """
+
+        self.size = size
