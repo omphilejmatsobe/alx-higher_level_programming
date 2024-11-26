03910e7a792b6ce63fbd0aa6b8f7947badfa8bcb
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-11-27 00:19:53 +0200

    Calculation module complete

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,10 +2,9 @@
 
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    import sys
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[2])
+    a = 10
+    b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
     import sys
 
-    if len(sys.argv) == 1:
-        print("{} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv) - 1))
+    if (len(sys.argv)) == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-
-    for x in range(1, len(sys.argv)):
-        print("{}: {}".format(x, sys.argv[x]))
+        if len(sys.argv) == 1:
+            print("0 arguments.")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+            for i in range(1, len(sys.argv)):
+                print("{}: {}".format(i, sys.argv[i]))
