47e2e3e492bb3770b273bc683278a201e7c8a056
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-11-27 01:20:32 +0200

    App update

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    for name in names:
+        if name.startswith("__"):
+            pass
+        else:
+            print(name)
