33ce5e739df37d6123ab6d1ab49d51d31c17bb7b
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-11-26 23:46:37 +0200

    Calculation module complete

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[2])
 
-    print(f"{a} + {b} = {add(a, b)}");
-    print(f"{a} - {b} = {sub(a, b)}");
-    print(f"{a} * {b} = {mul(a, b)}");
-    print(f"{a} / {b} = {div(a, b)}");
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
