60fdd184d03ac4e45d12a40839bb1d9a55ea3b14
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-11-27 00:25:40 +0200

    Calculation module complete

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
     import sys
 
-    sum = 0
+    sum = 0;
 
     for x in range(1, len(sys.argv)):
-        sum += int(sys.argv[x])
-
+            sum += sys.argv[x]
     print(sum)
